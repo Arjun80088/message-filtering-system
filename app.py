640979a3e54785e635b7a56fde4874a3924c5cdd
Arjun80088 <104100117+Arjun80088@users.noreply.github.com>
2022-12-09 15:40:06 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,47 @@
+from unittest import TestResult, result
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+from sklearn.feature_extraction.text import  CountVectorizer
+from sklearn.metrics import accuracy_score
+
+
+app = Flask(__name__)
+
+predictResult=pickle.load(open("spam.pkl","rb"))
+vectorizer = CountVectorizer()
+vectorizer.fit(predictResult[0])
+predictResult.pop(0)
+
+
+@app.route('/')
+def man():
+    return render_template("index.html")
+
+@app.route("/predict", methods=["POST","GET"])
+def home():
+    print(1)
+    msg=request.form.get("message")
+    msg=[msg]
+    xv=vectorizer.transform(msg)
+    TestResult=[]
+    c1=0
+    c2=0
+    classifierList=["Neural Network"]
+    j=0
+    for i in predictResult:
+        if classifierList[j]=="Gradient Boosting Classifier":
+            TestResult.append((classifierList[j],1)) 
+            j=j+1
+            TestResult.append((classifierList[j],1))
+            j=j+1
+            TestResult.append((classifierList[j],1))
+            break  
+        predictions = i.predict(xv)
+        TestResult.append((classifierList[j],predictions[0]))       
+        j=j+1
+    return render_template("index.html",data=TestResult)
+
+if __name__ =="__main__":
+    app.debug=True
+    app.run()
